900519a5fa21347ae9d0a6d8cf11b881121fdc12
xxm669 <xxm669@users.noreply.github.com>
2024-07-28 00:32:23 +0000

    chore: backup xiaoyahelper

--- xiaoyahelper/aliyun_clear.sh
+++ xiaoyahelper/aliyun_clear.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ver="202404270924"
+ver="202407272055"
 
 upgrade_url="https://xiaoyahelper.ddsrem.com/aliyun_clear.sh"
 upgrade_url_backup="http://xiaoyahelper.zngle.cf/aliyun_clear.sh"
@@ -154,17 +154,6 @@ if [ ! "$latest_ver"x = x ] && [ ! "$ver"x = "$latest_ver"x ]; then
         exit 0
     fi
 fi
-
-docker_mirror() {
-    mirrors="$(curl --insecure -fsSL https://ddsrem.com/xiaoya/all_in_one.sh | awk '/mirrors=\(/,/\)/' | sed -n 's/^[[:space:]]*"\(.*\)"[[:space:]]*$/\1/p' | grep -v "docker\.io" | grep -v "dockerproxy\.com" | grep -v "docker\.jsdelivr\.fyi")"
-    output="$(
-        for line in $mirrors; do
-            curl -s -o /dev/null -m 4 -w '%{time_total} '$line'\n' --head --request GET "$line" &
-        done
-        wait
-    )"
-    echo "$output" | sort -n | head -n1 | awk '{print $2}'
-}
 
 get_Header() {
     response=$(curl --connect-timeout 5 -m 5 -s -H "Content-Type: application/json" \
@@ -611,6 +600,28 @@ copy_tvbox_files() {
 copy_tvbox_files'
 }
 
+docker_pull() {
+    mirrors="$(curl --insecure -fsSL https://ddsrem.com/xiaoya/all_in_one.sh | awk '/mirrors=\(/,/\)/' | sed -n 's/^[[:space:]]*"\(.*\)"[[:space:]]*$/\1/p' | grep -v "docker\.io")"
+    mirrors="$(
+        for line in $mirrors; do
+            curl -s -o /dev/null -m 4 -w '%{time_total} '$line'\n' --head --request GET "$line" &
+        done
+        wait
+    )"
+    mirrors="$(echo "$mirrors" | sort -n | awk '{print $2}')"
+    for mirror in $mirrors; do
+        echo "尝试使用镜像源拉取：$mirror/$1"
+        docker tag "$1" "$mirror/$para_i" > /dev/null 2>&1
+        docker pull "$mirror/$1"
+        res=$?
+        docker tag "$mirror/$1" "$1" > /dev/null 2>&1
+        docker rmi "$mirror/$1" > /dev/null 2>&1
+        if [ $res -eq 0 ]; then
+            break
+        fi
+    done
+}
+
 update_xiaoya() {
     sleep 60
     para_v="$(docker inspect --format='{{range $v,$conf := .Mounts}}-v {{$conf.Source}}:{{$conf.Destination}} {{$conf.Type}}~{{end}}' $XIAOYA_NAME | tr '~' '\n' | grep bind | sed 's/bind//g' | grep -Eo "\-v .*:.*" | tr '\n' ' ')"
@@ -626,12 +637,7 @@ update_xiaoya() {
     para_i="$(get_docker_info $XIAOYA_NAME | awk '{print $2}'):$tag"
     para_e="$(docker inspect --format='{{range $p, $conf := .Config.Env}}~{{$conf}}{{end}}' $XIAOYA_NAME 2>/dev/null | sed '/^$/d' | tr '~' '\n' | sed '/^$/d' | awk '{printf("-e \"%s\"\n",$0)}' | tr '\n' ' ')"
     #docker pull "$para_i" 2>&1
-    mirror="$(docker_mirror)"
-    echo "当前使用镜像源是：$mirror"
-    docker tag "$para_i" "$mirror/$para_i" > /dev/null 2>&1
-    docker pull "$mirror/$para_i"
-    docker tag "$mirror/$para_i" "$para_i" > /dev/null 2>&1
-    docker rmi "$mirror/$para_i" > /dev/null 2>&1
+    docker_pull "$para_i"
     cur_image=$(get_docker_info $XIAOYA_NAME | awk '{print $3}')
     latest_image=$(docker images --no-trunc | tail -n +2 | tr ':' ' ' | awk '{printf("%s:%s %s\n",$1,$2,$4)}' | grep "$para_i" | awk '{print $2}')
 
@@ -744,6 +750,7 @@ install_keeper() {
     if para -b; then
         network=""
     fi
+    docker_pull "ddsderek/xiaoyakeeper:latest"
     docker run --name xiaoyakeeper --restart=always $network --privileged -v /var/run/docker.sock:/var/run/docker.sock -e TZ="Asia/Shanghai" -d ddsderek/xiaoyakeeper:latest sh -c "if [ -f /etc/xiaoya/aliyun_clear.sh ];then sh /etc/xiaoya/aliyun_clear.sh $1;else sleep 60;fi"
     docker exec xiaoyakeeper touch /docker-entrypoint.sh
     docker exec xiaoyakeeper sh -c "mkdir /etc/xiaoya > /dev/null 2>&1"
